05c7d4a52eacae1c2d69422fc919a7cf5fe0db3c
Leon Tur <tutrkov.leon-2@yandex.ru>
2022-11-08 00:25:27 +0300

    Finished Ex.3

--- 3.py
+++ 3.py
@@ -4,3 +4,22 @@
 # 47756688399943 -> [5]
 # 1113384455229 -> [8,9]
 # 1115566773322 -> []
+
+import string
+
+
+num = '1113384455229'
+list = []
+for _ in range(len(num)):
+    for i in range(len(num)):
+        if _ == i:
+            continue
+        elif num[_] == num[i]:
+            tmp = None
+            break
+        else:
+            tmp = (num[_])
+    if tmp is not None:
+        list.append([tmp])
+
+print (list)
